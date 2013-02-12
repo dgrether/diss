626c9385cd33b18da40277fb5dc9bd38672398e2
dgrether <dominik.grether@gmail.com>
2013-02-12 21:29:00 +0100

    added factory setter and getters to Calculator for better bean support

--- calculator/src/main/java/de/dgrether/diss/Calculator.java
+++ calculator/src/main/java/de/dgrether/diss/Calculator.java
@@ -1,7 +1,12 @@
 package de.dgrether.diss;
 
+import de.dgrether.diss.expressions.ExpressionFactory;
+
 public interface Calculator {
 	
 	public int calculate();
 
+	public ExpressionFactory getFactory();
+
+	public void setFactory(ExpressionFactory factory);
 }
